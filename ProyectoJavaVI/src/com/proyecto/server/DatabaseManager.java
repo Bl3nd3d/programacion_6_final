495d00a990038f493a13192f4b254665b6a14410
Martinvera627 <168845819+Bl3nd3d@users.noreply.github.com>
2026-07-27 01:47:25 -0300

    .

--- ProyectoJavaVI/src/com/proyecto/server/DatabaseManager.java
+++ ProyectoJavaVI/src/com/proyecto/server/DatabaseManager.java
@@ -1,9 +1,14 @@
 package com.proyecto.server;
 
-import com.proyecto.shared.Task;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.proyecto.shared.Task;
 
 public class DatabaseManager {
     private static DatabaseManager instance;
